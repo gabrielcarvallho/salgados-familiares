4bc238aa67c66cc68dc34055ab58770be5ed2a7d
imrickss <henriqueluiz16022000@gmail.com>
2025-05-04 10:36:21 -0300

    hotfix

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,16 +1,15 @@
+// app/register/page.tsx
 // NÃO coloque "use client" aqui
-export const dynamic = "force-dynamic"; // garante que a página seja sempre dinâmica
+export const dynamic = "force-dynamic";
 
 import RegisterFormClient from "./RegisterFormClient";
 
-interface RegisterPageProps {
-  searchParams: {
-    token?: string
-  }
-}
-
-export default function RegisterPage({ searchParams }: RegisterPageProps) {
-  const token = searchParams.token || "";
+export default function RegisterPage({
+  searchParams,
+}: {
+  searchParams: { token?: string };
+}) {
+  const token = searchParams.token ?? "";
 
   return <RegisterFormClient token={token} />;
 }
